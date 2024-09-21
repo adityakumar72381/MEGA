2a5e992a8d180e2514f6dee5d433c6c413e998f3
adityakumar72381 <adityakumar72381@gmail.com>
2024-09-22 01:25:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-    app.run_polling()
+    app.run_polling(port=8443)
 
 if __name__ == '__main__':
     main()
